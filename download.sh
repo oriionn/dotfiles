306a381693a753e50a892ea89fedbef13d878347
Orion <contact@oriondev.fr>
2026-04-03 20:50:50 +0200

    chore: bump kdrive version

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
 mkdir -p bin/
-curl -L "https://download.storage.infomaniak.com/drive/desktopclient/kDrive-3.7.6.20250908-amd64.AppImage" -o bin/kDrive.AppImage
+curl -L "https://download.storage.infomaniak.com/drive/desktopclient/kDrive-3.7.10.1-amd64.AppImage" -o bin/kDrive.AppImage
