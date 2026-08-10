d7789c7f4ce1784aa9496abd49b63e2a9fc32f04
Orion <contact@oriondev.fr>
2026-08-10 19:58:38 +0200

    fix: hyprsplit

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,11 +1,5 @@
-{ ... }:
+{ pkgs, ... }:
 
-let
-    hyprsplit = builtins.fetchGit {
-        url = "https://github.com/shezdy/hyprsplit";
-        ref = "main";
-    };
-in
 {
     imports = [
         ../config/ghostty/config.nix
@@ -21,24 +15,8 @@ in
     wayland.windowManager.hyprland = {
         enable = true;
 
-        extraLuaFiles = {
-            # ~/.config/hypr/hyprsplit/init.lua
-            "hyprsplit/init" = {
-                autoLoad = false;
-                content = builtins.readFile "${hyprsplit}/init.lua";
-            };
-
-            # Chargé automatiquement par Hyprland
-            "hyprsplit-config" = {
-                autoLoad = true;
-                content = ''
-                    local hs = require("hyprsplit")
-
-                    hs.config({
-                        num_workspaces = 10
-                    })
-                '';
-            };
-        };
+        plugins = [
+            pkgs.hyprlandPlugins.hyprsplit
+        ];
      };
 }
