ece1f24f57c839a1d8c6ff6abb483013cb86624e
Orion <contact@oriondev.fr>
2026-08-10 18:51:26 +0200

    feat: use unstable for hyprland

--- nixos/desktop/hyprland.nix
+++ nixos/desktop/hyprland.nix
@@ -1,6 +1,16 @@
 { ... }:
 
+let
+  unstable = import (builtins.fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz"
+  ) {};
+in
 {
-    programs.hyprland.enable = true;
+    programs.hyprland = {
+        enable = true;
+
+        package = unstable.hyprland;
+        portalPackage = unstable.xdg-desktop-portal-hyprland;
+    };
     environment.sessionVariables.NIXOS_OZONE_WL = "1"; # hint electron apps to use wayland
 }
