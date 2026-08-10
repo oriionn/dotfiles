8833b21e1135d206964d5aa32f169f8d6f7fec41
Orion <contact@oriondev.fr>
2026-08-10 16:34:50 +0200

    feat: hyprland, kde, ly base setup

--- nixos/config.nix
+++ nixos/config.nix
@@ -1,12 +1,29 @@
 { config, pkgs, ... }:
 
 {
+    imports =
+     [
+         ./hyprland.nix
+     ]
+
+    # Enable unfree packages
     nixpkgs.config.allowUnfree = true;
+
+    # Packages
     environment.systemPackages = with pkgs; [
         git
         neovim
     ];
 
+    # KDE
+    services = {
+        desktopManager.plasma6.enable = true;
+    };
+
+    # Ly
+    services.displayManager.ly.enable = true;
+
+    # Git configuration
     programs.git = {
         enable = true;
         config = {
