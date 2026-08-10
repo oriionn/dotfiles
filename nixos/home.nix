44cd19b4e27fb8304090b7377b22074ca3758cf1
Orion <contact@oriondev.fr>
2026-08-10 19:54:10 +0200

    fix: semicolon

--- nixos/home.nix
+++ nixos/home.nix
@@ -14,8 +14,8 @@ in
     home.stateVersion = "26.05";
 
     # Config files
-    xdg.configFile."hypr/hyprland.conf".source = ../config/hypr/hyprland.conf
-    xdg.configFile."hypr/hyprpaper.conf".source = ../config/hypr/hyprpaper.conf
+    xdg.configFile."hypr/hyprland.conf".source = ../config/hypr/hyprland.conf;
+    xdg.configFile."hypr/hyprpaper.conf".source = ../config/hypr/hyprpaper.conf;
 
     # Hyprsplit
     wayland.windowManager.hyprland = {
