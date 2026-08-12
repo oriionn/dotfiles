7af7ec121604fbd7f706aa6b415c044e449d2915
Orion <contact@oriondev.fr>
2026-08-12 02:36:03 +0200

    fix: remove custom kdeglobals

--- config/hypr/hyprland.nix
+++ config/hypr/hyprland.nix
@@ -513,13 +513,4 @@
     };
 
     xdg.configFile."hypr/xdph.conf".source = ./xdph.conf;
-
-
-    xdg.configFile."kdeglobals".text = ''
-        [KDE]
-        widgetStyle=Breeze
-
-        [General]
-        ColorScheme=BreezeDark
-    '';
 }
