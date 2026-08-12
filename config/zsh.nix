8cae03670515d6a96447daf1bf3ec09b11afff91
Orion <contact@oriondev.fr>
2026-08-12 20:28:32 +0200

    fix: bindkey

--- config/zsh.nix
+++ config/zsh.nix
@@ -28,7 +28,7 @@
             findNoDups = true;
          };
 
-        interactiveShellInit = ''
+        initContent = ''
             bindkey '^[[A' history-search-backward
             bindkey '^[[B' history-search-forward
         '';
