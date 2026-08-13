a34577e3fb2160cb2d0ba1d74ca728be0ae0d903
Orion <contact@oriondev.fr>
2026-08-13 23:13:03 +0200

    feat: ly gnome key ring

--- nixos/config.nix
+++ nixos/config.nix
@@ -30,7 +30,7 @@ in
 
     # Keyring
     services.gnome.gnome-keyring.enable = true;
-    security.pam.services.login.enableGnomeKeyring = true;
+    security.pam.services.ly.enableGnomeKeyring = true;
 
     # Bluetooth
     hardware.bluetooth = {
