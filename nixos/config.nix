f8efd2d14c7ba0a1b7d1aaf1daacbd17a9b2078d
Orion <contact@oriondev.fr>
2026-08-14 15:01:14 +0200

    feat: nameservers

--- nixos/config.nix
+++ nixos/config.nix
@@ -27,6 +27,11 @@ in
 
     # Enable networking
     networking.networkmanager.enable = true;
+    networking.nameservers = [
+        "194.242.2.2" # Mullvad
+        "1.1.1.1" # Cloudflare
+        "1.0.0.1" # Secondary Cloudflare
+    ]
 
     # Keyring
     services.gnome.gnome-keyring.enable = true;
