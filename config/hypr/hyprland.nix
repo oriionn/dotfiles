10ae833dc859e6067c44c77892761fc62f4abc24
Orion <contact@oriondev.fr>
2026-08-14 22:43:55 +0200

    fix: use xdg.configFile because home manager 26.05 is not updated for hyprland lua...

--- config/hypr/hyprland.nix
+++ config/hypr/hyprland.nix
@@ -10,21 +10,16 @@
         portalPackage = unstable.xdg-desktop-portal-hyprland;
 
         xwayland.enable = true;
-
         configType = "lua";
-        extraLuaFiles = {
-            # create a symlink to `.config/hypr/hyprsplit/init.lua`.
-            "hyprsplit/init" = {
-                autoLoad = false;
-                content = builtins.readFile "${hyprsplit}/init.lua";
-            };
-            # Finally, use it directly in Lua.
-            "hyprload" = {
-                autoLoad = true;
-                content = ./hyprland.lua;
-            };
-        };
     };
+    xdg.configFile."hypr/hyprsplit/init.lua" = {
+        force = true;
+        content = builtins.readFile "${hyprsplit}/init.lua";
+    }
+    xdg.configFile."hypr/hyprland.lua" = {
+        force = true;
+        source = ./hyprland.lua;
+    }
 
     xdg.configFile."hypr/xdph.conf".source = ./xdph.conf;
 }
