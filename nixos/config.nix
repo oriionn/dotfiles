c76e323c227ecac0853dc8e44b0fdf62f77774ad
Orion <contact@oriondev.fr>
2026-08-15 22:26:00 +0200

    fix: oopsie

--- nixos/config.nix
+++ nixos/config.nix
@@ -105,9 +105,6 @@ in
         openFirewall = true;
     };
 
-    # Auto CPUFreq (managing cpu to save battery)
-    services.auto-cpufreq.enable = true;
-
     # Packages
     environment.systemPackages = with pkgs; [
         # Terminal
